2ee2e6df442ae92d2712e0d9b55844547617c424
delesslin <delesslingw@gmail.com>
2021-10-14 23:34:30 -0400

    :wrench: adding dotenv to hopefully help with heroku deployment error

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000
 app.use(express.static(path.join(__dirname, '../client/web-build')))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/web-build/index.html'))
 })
 
-app.listen(8000, (err) => {
+app.listen(PORT, (err) => {
   if (err) console.error(err)
   console.log('Listening at http://localhost:8000')
 })
