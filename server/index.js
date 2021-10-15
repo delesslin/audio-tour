17aa5541ec30eac94f323154828ee8f6998a4884
delesslin <delesslingw@gmail.com>
2021-10-14 23:27:54 -0400

    :wrench: adding env PORT for heroku depoloyment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const port = process.env.PORT || 8000
 app.use(express.static(path.join(__dirname, '../client/web-build')))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/web-build/index.html'))
