471968665aa4e8dc7fa68f3c54a3704c49581b7f
delesslin <delesslingw@gmail.com>
2021-10-15 19:32:58 -0400

    playing

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 8000
 app.use(express.static(path.join(__dirname, '../client/web-build')))
-app.get('*', (req, res) => {
+app.get('*/*', (req, res) => {
   console.log(req.body)
   res.sendFile(path.join(__dirname, '../client/web-build/index.html'))
 })
