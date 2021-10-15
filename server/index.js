94e569618ff8a0e504a048e538dd02ac52fff870
delesslin <delesslingw@gmail.com>
2021-10-15 19:28:35 -0400

    adding console.log to app.get

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 8000
 app.use(express.static(path.join(__dirname, '../client/web-build')))
 app.get('*', (req, res) => {
+  console.log(req.body)
   res.sendFile(path.join(__dirname, '../client/web-build/index.html'))
 })
 
