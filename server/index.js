679d31ae62d2410c348eb9267d05ca593da4f12f
delesslin <delesslingw@gmail.com>
2021-11-03 22:41:20 -0400

    :wrench: creating endpoint for download links /api/download

--- server/index.js
+++ server/index.js
@@ -8,20 +8,17 @@ const axios = require('axios')
 const app = express()
 const PORT = process.env.PORT || 8000
 let apiURL = 'https://catawba-audio-tour.s3.us-east-2.amazonaws.com'
+const { ANDROID_URL, APPLE_URL } = process.env
 
 // app.use(cors())
 app.use(express.static(path.join(__dirname, '../client/web-build')))
+app.get('/api/download/android', (req, res) => {
+  res.send(ANDROID_URL)
+})
+app.get('/api/download/apple', (req, res) => {
+  res.send(APPLE_URL)
+})
 app.use('/api', proxy(apiURL))
-// app.get('/api/*', (req, res) => {
-//   const remoteURL = req.params[0]
-//   axios(remoteURL).then(({ data }) => {
-//     if (remoteURL.includes('image') || remoteURL.includes('audio')) {
-//       res.sendFile(data)
-//       return
-//     }
-//     res.send(data)
-//   })
-// })
 app.get('*', (req, res) => {
   console.log('GET * request', req.body)
   res.sendFile(path.join(__dirname, '../client/web-build/index.html'))
