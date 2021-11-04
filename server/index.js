118461ab7ff2180bc73d0361cd5ec1ff9e02cd69
delesslin <delesslingw@gmail.com>
2021-11-03 22:42:19 -0400

    :wrench: creating endpoint for download links /api/download

--- server/index.js
+++ server/index.js
@@ -1,16 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const path = require('path')
-// const cors = require('cors')
-// const fetch = require('node-fetch')
 const proxy = require('express-http-proxy')
-const axios = require('axios')
+
 const app = express()
 const PORT = process.env.PORT || 8000
 let apiURL = 'https://catawba-audio-tour.s3.us-east-2.amazonaws.com'
 const { ANDROID_URL, APPLE_URL } = process.env
 
-// app.use(cors())
 app.use(express.static(path.join(__dirname, '../client/web-build')))
 app.get('/api/download/android', (req, res) => {
   res.send(ANDROID_URL)
