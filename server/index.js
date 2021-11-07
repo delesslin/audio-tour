7959fbee0a84f07243f51bbc6ee01475d3b23960
delesslin <delesslingw@gmail.com>
2021-11-07 18:26:52 -0500

    :trash: removing qr info

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 const proxy = require('express-http-proxy')
 const app = express()
-const generateQR = require('./generateQR')
+
 const PORT = process.env.PORT || 8001
 let apiURL = 'https://catawba-audio-tour.s3.us-east-2.amazonaws.com'
 const { ANDROID_URL, APPLE_URL, DATA_UPDATE_INTERVAL_MS } = process.env
