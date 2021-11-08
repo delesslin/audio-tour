dad83e74deb517b98fe8fb7da2afa028cc1ea251
delesslin <delesslingw@gmail.com>
2021-11-07 22:34:20 -0500

    :trash: removing proxy from server

--- server/index.js
+++ server/index.js
@@ -2,17 +2,23 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 // TODO: is proxy still needed?
-const proxy = require('express-http-proxy')
+
 const app = express()
 
 const PORT = process.env.PORT || 8001
-let apiURL = 'https://catawba-audio-tour.s3.us-east-2.amazonaws.com'
+// let apiURL = 'https://catawba-audio-tour.s3.us-east-2.amazonaws.com'
 const { DATA_UPDATE_INTERVAL_MS } = process.env
 let DATA = {}
 let STATIC = {}
 const getData = require('./getData')
 const getStatic = require('./getStatic')
+// TODO: https redirect
+// http.get('*', function(req, res) {
+//   res.redirect('https://' + req.headers.host + req.url);
 
+//   // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+//   // res.redirect('https://example.com' + req.url);
+// })
 app.use(express.static(path.join(__dirname, '../client/web-build')))
 
 app.get('/api/data', async (req, res) => {
@@ -26,7 +32,7 @@ app.get('/privacy', (req, res) => {
     Catawba Nation's Audio Tour server and websites/apps ("clients") do not collect personal data from site visitors or app users ("users"). When users access the Audio Tour through a client generic requests are made to our server. Any unintentional data that is included in that request by default (e.g. source IP) is not used or collected by the server. The app is free to download, free to use, and does not contain any premium options. For questions regarding this policy, please contact us at info@catawbaculture.org
   `)
 })
-app.use('/api', proxy(apiURL))
+// app.use('/api', proxy(apiURL))
 app.get('*', (req, res) => {
   console.log('GET * request', req.body)
   res.sendFile(path.join(__dirname, '../client/web-build/index.html'))
